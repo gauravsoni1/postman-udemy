95c529c6c904a6e843cd406529ad72f0d9249c4b
Gaurav soni <Gaurav@allcadservices.com>
2022-08-28 21:25:52 +0400

    Fixed env config for windows

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
-const path = require("path");
 
-const serverCofig = JSON.parse(fs.readFileSync(path.resolve(__dirname + `/../${process.env.NODE_ENV}` + ".json"), { encoding: "utf-8" }));
+const serverCofig = JSON.parse(fs.readFileSync(process.env.NODE_ENV + ".json"), { encoding: "utf-8" });
 
 module.exports = serverCofig;
